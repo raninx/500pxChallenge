b7294d7c8508514920e20fc75826e3f6f5d24f62
raninx <rani.nx@gmail.com>
2019-05-25 23:56:52 -0400

    Getting image url from response model

--- app/src/main/java/com/project/a500pxchallenge/model/Photo.java
+++ app/src/main/java/com/project/a500pxchallenge/model/Photo.java
@@ -1,6 +1,15 @@
 package com.project.a500pxchallenge.model;
 
+import java.util.ArrayList;
+
 public class Photo {
     public String current_page;
+    public ArrayList<Details> photos;
+    public class Details {
+        public ArrayList<Url> images;
+    }
+    public class Url {
+        public String https_url;
+    }
 
 }
